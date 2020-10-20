869c41ce6d850794bc4e2162e82a3011e64e653f
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-20 20:02:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-    console.log(array.unshift(element))
+    array.unshift(element)
 }
